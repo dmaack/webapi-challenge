31fbef9afffdc019ae4a365674903eb39b6262fb
Dominique Maack <dominiquemaack@Dominiques-MacBook-Pro.local>
2019-11-01 10:46:50 -0600

    UPDATE and DELETE complete for actions

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -31,19 +31,49 @@ actionRouter.get('/', (req,res) => {
 
 actionRouter.get('/project_id', (req,res) => {
     const project_id = req.params.id;
-
+    
     
 })
 
 // UPDATE Requests
 
+actionRouter.put('/:id', (req,res) => {
+    const id = req.params.id;
+    const updates = req.body;
 
+    if(!updates.description || !updates.notes) {
+        res.status(400).json({ error: 'Please provide a description AND notes for the action'})
+    } else {
+        actionModel.update(id, updates)
+        .then(action => {
+            if(action) {
+                res.status(201).json(action)
+            } else {
+                res.status(500).json({ error: "There was an error while saving the description or note to the database" })
+            }
+        })
+        .catch(() => {
+           res.status(500).json({ error: "Server error" })
+        })
+    }
+
+})
 
 
 
 // DELETE Requests
 
+actionRouter.delete('/:id', (req,res) => {
+    const id = req.params.id;
 
+    actionModel.remove(id)
+    .then(() => {
+        res.status(200).send(`You successfully deleted the action with ID ${id}`)
+    })
+    .catch(() => {
+        res.status(500).json({error: 'Server error'})
+    })
+})
 
 // Middleware 
 
